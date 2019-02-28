6fe95d13abb0abd99ad86a84c6d52b86fb5de98c
SootyForge <42379545+SootyForge@users.noreply.github.com>
2019-02-28 15:56:54 +1100

    Week 4 - 002
    Minesweeper3D

---
+++ Assets/Minesweeper3D/Scripts/Grid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper3D
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10, height = 10, depth = 10;
+        public float spacing = 1.1f;
+
+        // 3D Array to store all the tiles
+        private Tile[,,] tiles;
+
+        // Use this for initialization
+        void Start()
+        {
+            GenerateTiles();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void GenerateTiles()
+        {
+            // Instantiate the new 3D array of size width × height × depth
+            tiles = new Tile[width, height, depth];
+
+            // Store half the size of the grid
+            Vector3 halfSize = new Vector3(width * .5f, height * .5f, depth * .5f);
+
+            // Offset
+            Vector3 offset = new Vector3(.5f, .5f, .5f);
+
+            // Loop through the entire list of tiles
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int z = 0; z < depth; z++)
+                    {
+                        // Generate position for current tile
+                        Vector3 position = new Vector3(x - halfSize.x,
+                                                       y - halfSize.y,
+                                                       z - halfSize.z);
+                        // Offset position to center
+                        position += offset;
+                        // Apply spacing
+                        position *= spacing;
+                        // Spawn the tile
+                        Tile newTile = SpawnTile(position);
+                        // Store coordinates
+                        newTile.x = x;
+                        newTile.y = y;
+                        newTile.z = z;
+                        // Store tile in array at those coordinates
+                        tiles[x, y, z] = newTile;
+                    }
+                }
+            }
+        }
+
+        Tile SpawnTile(Vector3 position)
+        {
+            // Clone the tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+            // Edit it's properties
+            clone.transform.position = position;
+            // Return the Tile component of clone
+            return clone.GetComponent<Tile>();
+        }
+    } 
+}
---
+++ Assets/Minesweeper3D/Scripts/Tile.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+namespace Minesweeper3D
+{
+    public class Tile : MonoBehaviour
+    {
+        public int x, y, z;
+        public bool isMine = false;
+        public bool isRevealed = false;
+        public GameObject minePrefab;
+        public GameObject textPrefab;
+        [Range(0, 1)]
+        public float mineChance = 0.15f;
+
+        private Animator anim;
+        private GameObject mine;
+        private GameObject text;
+        private Collider col;
+
+        private void Awake()
+        {
+            // Get reference
+            anim = GetComponent<Animator>();
+            col = GetComponent<Collider>();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            // Set mine chance
+            isMine = Random.value < mineChance;
+            // Check if it's a mine
+            if(isMine)
+            {
+                // Create instance of mine gameobject
+                mine = Instantiate(minePrefab, transform);
+                mine.SetActive(false);
+            }
+            else
+            {
+                // Create instance of text gameobject
+                text = Instantiate(textPrefab, transform);
+                text.SetActive(false);
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            Reveal(10);
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            // Flags the tile as being revealed
+            isRevealed = true;
+            // Run Reveal animation
+            anim.SetTrigger("Reveal");
+            // Disable collision
+            col.enabled = false;
+            // Check if tile is mine
+            if (isMine)
+            {
+                // Activate mine
+                mine.SetActive(true);
+            }
+            else
+            {
+                // Enabling text
+                text.SetActive(true);
+                // Setting text
+                text.GetComponent<TextMeshPro>().text = adjacentMines.ToString();
+            }
+        }
+    } 
+}
